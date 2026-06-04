e8032b930f0816964a80ebe4ddf29795951e292f
ilhom nomozov <nomozof@IlhomMacBook.local>
2026-06-05 01:57:03 +0900

    feat: typeorm config, validation pipe, port 4000

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host:     config.get('DB_HOST'),
+        port:     config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities:     [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize:  true,  // dev uchun, prod da false
+        logging:      false,
+      }),
+      inject: [ConfigService],
+    }),
+
+    ScheduleModule.forRoot(),
+  ],
 })
 export class AppModule {}
