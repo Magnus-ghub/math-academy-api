de786a306b275ac8281d1e8d6720209a6aefc3c0
Magnus <magnus.kordev@gmail.com>
2026-07-18 21:59:33 +0900

    fix: change domen

--- src/main.ts
+++ src/main.ts
@@ -36,8 +36,8 @@ async function bootstrap() {
         return callback(null, true);
       }
       const allowed = [
-        'https://cuben.info',
-        'https://www.cuben.info',
+        'https://edufly.uz',
+        'https://www.edufly.uz',
         process.env.FRONTEND_URL,
       ].filter(Boolean);
       if (allowed.includes(origin)) {
